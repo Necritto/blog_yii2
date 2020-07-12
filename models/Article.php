22b0ce03074065e8468e8bd72030c4abb916f767
Necritto <onetopyr@mail.ru>
2020-07-12 15:28:48 +0300

    Feature: set image [ref:no-ref]

--- models/Article.php
+++ models/Article.php
@@ -2,8 +2,6 @@
 
 namespace app\models;
 
-use Yii;
-
 /**
  * This is the model class for table "article".
  *
@@ -23,64 +21,67 @@ use Yii;
  */
 class Article extends \yii\db\ActiveRecord
 {
-    /**
-     * {@inheritdoc}
-     */
-    public static function tableName()
-    {
-        return 'article';
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public static function tableName()
+  {
+    return 'article';
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-            [['title', 'description', 'content'], 'required'],
-            [['title'], 'string', 'max' => 255],
-            [['title', 'description', 'content'], 'string'],
-            [['date'], 'date', 'format' => 'php:Y-m-d'],
-            [['date'], 'default', 'value' => date('Y-m-d')],
-        ];
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function rules()
+  {
+    return [
+      [['title', 'description', 'content'], 'required'],
+      [['title'], 'string', 'max' => 255],
+      [['title', 'description', 'content'], 'string'],
+      [['date'], 'date', 'format' => 'php:Y-m-d'],
+      [['date'], 'default', 'value' => date('Y-m-d')],
+    ];
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function attributeLabels()
-    {
-        return [
-            'id' => 'ID',
-            'title' => 'Title',
-            'description' => 'Description',
-            'content' => 'Content',
-            'date' => 'Date',
-            'image' => 'Image',
-            'viewed' => 'Viewed',
-            'user_id' => 'User ID',
-            'status' => 'Status',
-            'category_id' => 'Category ID',
-        ];
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function attributeLabels()
+  {
+    return [
+      'id' => 'ID',
+      'title' => 'Title',
+      'description' => 'Description',
+      'content' => 'Content',
+      'date' => 'Date',
+      'image' => 'Image',
+      'viewed' => 'Viewed',
+      'user_id' => 'User ID',
+      'status' => 'Status',
+      'category_id' => 'Category ID',
+    ];
+  }
 
-    /**
-     * Gets query for [[ArticleTags]].
-     *
-     * @return \yii\db\ActiveQuery
-     */
-    public function getArticleTags()
-    {
-        return $this->hasMany(ArticleTag::className(), ['article_id' => 'id']);
-    }
+  public function saveImage($filename)
+  {
+    $this->image = $filename;
+    $this->save(false);
+  }
 
-    /**
-     * Gets query for [[Comments]].
-     *
-     * @return \yii\db\ActiveQuery
-     */
-    public function getComments()
-    {
-        return $this->hasMany(Comment::className(), ['article_id' => 'id']);
-    }
+  public function getImage()
+  {
+    return $this->image ? '/uploads/' . $this->image : '/no-image.png';
+  }
+
+  public function deleteImage()
+  {
+    $imageUploadedModel = new ImageUpload();
+    $imageUploadedModel->deleteCurrentImage($this->image);
+  }
+
+  public function beforeDelete()
+  {
+    $this->deleteImage();
+    return parent::beforeDelete();
+  }
 }
